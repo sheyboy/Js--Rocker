7cf35f30d6d43665aee1f51748cf368209229331
Supergamma <iamsheyie@gmail.com>
2022-08-21 17:37:26 +0100

    test

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var car = {
+				make: 'VW',
+				type: 'Polo',
+				color: 'blue',
+				isTurnedOn: false,
+				numberOfWheels: 4,
+				seats: ['seat 1',
+						'seat 2',
+						'seat 3',
+						'seat 4'],
+
+			
+			turnon: function (){
+
+				this.isTurnedOn = true;
+			}
+
+			};
